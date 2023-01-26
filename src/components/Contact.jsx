4046b900e9c2658c71540dff8f8402dffffdecea
Priya Mittal <priya.20scse1010889@galgotiasuniversity.edu.in>
2023-01-26 17:10:25 +0530

    second commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
 
         <div className=" flex justify-center items-center">
           <form
-            action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
+            action="https://getform.io/f/dd97923f-4033-4f91-882d-8dd5105bbfa7"
             method="POST"
             className=" flex flex-col w-full md:w-1/2"
           >
